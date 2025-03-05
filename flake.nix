d135116fb23c41a9f46d005d9836984f42d766a3
Florian Hartung <florian.hartung5@gmail.com>
2025-03-05 16:01:59 +0100

    build: setup nix

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Exploring WebAssembly for versatile plugin system through the example of a text editor";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    typst-packages = {
+      url = "github:typst/packages";
+      flake = false;
+    };
+    typix = {
+      url = "github:loqusion/typix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    
+  };
+  outputs = {self, nixpkgs, devshell, utils, ...}@inputs:
+    utils.lib.eachSystem ["x86_64-linux"] (system:
+      let
+        lib = nixpkgs.lib;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
+        typstPackagesCache = pkgs.stdenv.mkDerivation {
+          name = "typst-packages-cache";
+          src = inputs.typst-packages;
+          dontBuild = true;
+          installPhase = ''
+            mkdir -p "$out/typst/packages"
+            cp --dereference --no-preserve=mode --recursive --reflink=auto \
+              --target-directory="$out/typst/packages" -- "$src"/packages/*
+          '';
+        };
+      in
+      {
+        packages.main = inputs.typix.lib.${system}.buildTypstProject {
+          name = "main.pdf";
+          src = ./work/main.typ;
+          XDG_CACHE_HOME = typstPackagesCache;
+        };
+
+        devShells.default = (pkgs.devshell.mkShell {
+          name = "work";
+          packages = with pkgs; [
+            typst
+          ];
+          commands = [
+            {
+              name = "watch";
+              command = ''
+                typst watch --root "$PRJ_ROOT/work" "$PRJ_ROOT/work/main.typ"
+              '';
+              help = "watch the main typst project and recompile when changes are detected";
+            }
+          ];
+        });
+      });
+  # });
+}
